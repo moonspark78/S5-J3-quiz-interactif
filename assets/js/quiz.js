a4545ace56dd5597845d2ff921b993cb02d78b65
Antonin Grenier <drixion05@gmail.com>
2025-07-24 16:14:43 +0200

    questions sorting by time limit feature completed

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,172 +1,173 @@
 //quiz.js
 import {
-  getElement,
-  showElement,
-  hideElement,
-  setText,
-  createAnswerButton,
-  updateScoreDisplay,
-  lockAnswers,
-  markCorrectAnswer,
-  createSummaryTable, // Importez la nouvelle fonction
+	getElement,
+	showElement,
+	hideElement,
+	setText,
+	createAnswerButton,
+	updateScoreDisplay,
+	lockAnswers,
+	markCorrectAnswer,
+	createSummaryTable, // Importez la nouvelle fonction
 } from "./dom.js";
 
 import {
-  loadFromLocalStorage,
-  saveToLocalStorage,
-  startTimer,
+	loadFromLocalStorage,
+	saveToLocalStorage,
+	startTimer,
 } from "./utils.js";
 
 console.log("Quiz JS loaded...");
 
 const uiText = {
-  fr: {
-    title: "Quiz Dynamique",
-    introNotice: "Testez vos connaissances en quelques questions chronométrées !",
-    bestScore: "Meilleur score",
-    start: "Commencer le quiz",
-    question: "Question",
-    timeLeft: "Temps restant",
-    next: "Question suivante",
-    resultTitle: "Résultat final",
-    yourScore: "Votre score",
-    restart: "Recommencer",
-    selectLanguage: "Choisir la langue",
-    summaryTableTitle: "Récapitulatif des réponses", // Nouveau texte
-    summaryQuestion: "Question", // Nouveau texte
-    summaryYourAnswer: "Votre réponse", // Nouveau texte
-    summaryCorrectAnswer: "Bonne réponse", // Nouveau texte
-  },
-  en: {
-    title: "Dynamic Quiz",
-    introNotice: "Test your knowledge with a few timed questions!",
-    bestScore: "Best score",
-    start: "Start the quiz",
-    question: "Question",
-    timeLeft: "Time left",
-    next: "Next question",
-    resultTitle: "Final result",
-    yourScore: "Your score",
-    restart: "Restart",
-    selectLanguage: "Select language",
-    summaryTableTitle: "Answer Summary", // New text
-    summaryQuestion: "Question", // New text
-    summaryYourAnswer: "Your Answer", // New text
-    summaryCorrectAnswer: "Correct Answer", // New text
-  },
+	fr: {
+		title: "Quiz Dynamique",
+		introNotice:
+			"Testez vos connaissances en quelques questions chronométrées !",
+		bestScore: "Meilleur score",
+		start: "Commencer le quiz",
+		question: "Question",
+		timeLeft: "Temps restant",
+		next: "Question suivante",
+		resultTitle: "Résultat final",
+		yourScore: "Votre score",
+		restart: "Recommencer",
+		selectLanguage: "Choisir la langue",
+		summaryTableTitle: "Récapitulatif des réponses", // Nouveau texte
+		summaryQuestion: "Question", // Nouveau texte
+		summaryYourAnswer: "Votre réponse", // Nouveau texte
+		summaryCorrectAnswer: "Bonne réponse", // Nouveau texte
+	},
+	en: {
+		title: "Dynamic Quiz",
+		introNotice: "Test your knowledge with a few timed questions!",
+		bestScore: "Best score",
+		start: "Start the quiz",
+		question: "Question",
+		timeLeft: "Time left",
+		next: "Next question",
+		resultTitle: "Final result",
+		yourScore: "Your score",
+		restart: "Restart",
+		selectLanguage: "Select language",
+		summaryTableTitle: "Answer Summary", // New text
+		summaryQuestion: "Question", // New text
+		summaryYourAnswer: "Your Answer", // New text
+		summaryCorrectAnswer: "Correct Answer", // New text
+	},
 };
 
 const translations = {
-  fr: [
-    {
-      text: "Quelle est la capitale de la France ?",
-      answers: ["Marseille", "Paris", "Lyon", "Bordeaux"],
-      correct: 1,
-      timeLimit: 10,
-    },
-    {
-      text: "Combien font 2 + 3 ?",
-      answers: ["3", "4", "5", "1"],
-      correct: 2,
-      timeLimit: 5,
-    },
-    {
-      text: "Quelle est la capitale de la Belgique ?",
-      answers: ["Bruxelles", "Anvers", "Liège", "Namur"],
-      correct: 0,
-      timeLimit: 10,
-    },
-    {
-      text: "Quelle est la couleur du cheval blanc d'Henry IV ?",
-      answers: ["Noir", "Blanc", "Rouge", "Vert", "Blanche"],
-      correct: 4,
-      timeLimit: 5,
-    },
-    {
-      text: "Quelle est la capitale de la Mongolie ?",
-      answers: ["Ulanbator", "Shanghai", "Sainte-Anne", "Omsk"],
-      correct: 0,
-      timeLimit: 10,
-    },
-    {
-      text: "Quel nombre n'est pas un nombre premier ?",
-      answers: ["2", "11", "17", "54"],
-      correct: 3,
-      timeLimit: 15,
-    },
-    {
-      text: "Comment s'appelle le protagoniste Goa'Uld dans l'équipage SG-1 ?",
-      answers: ["Jarod", "Spock", "Teal'c", "Uld'Gald"],
-      correct: 2,
-      timeLimit: 10,
-    },
-    {
-      text: "Qui est le dernier président de la 3eme république Française ?",
-      answers: [
-        "Albert Lebrun",
-        "René Coty",
-        "Félix Faure",
-        "François Mitterrand",
-      ],
-      correct: 0,
-      timeLimit: 15,
-    },
-  ],
-  en: [
-    {
-      text: "What is the capital of France?",
-      answers: ["Marseille", "Paris", "Lyon", "Bordeaux"],
-      correct: 1,
-      timeLimit: 10,
-    },
-    {
-      text: "How much is 2 + 3?",
-      answers: ["3", "4", "5", "1"],
-      correct: 2,
-      timeLimit: 5,
-    },
-    {
-      text: "What is the capital of Belgium?",
-      answers: ["Brussels", "Antwerp", "Liege", "Namur"],
-      correct: 0,
-      timeLimit: 10,
-    },
-    {
-      text: "What is the color of the white horse of Henry IV?",
-      answers: ["Black", "White", "Red", "Green"],
-      correct: 1,
-      timeLimit: 5,
-    },
-    {
-      text: "What is the capital of Mongolia?",
-      answers: ["Ulan Bator", "Shanghai", "Sainte-Anne", "Omsk"],
-      correct: 0,
-      timeLimit: 10,
-    },
-    {
-      text: "What is not a prime number?",
-      answers: ["2", "11", "17", "54"],
-      correct: 3,
-      timeLimit: 15,
-    },
-    {
-      text: "What is the name of the protagonist Goa'Uld in the SG-1 fleet?",
-      answers: ["Jarod", "Spock", "Teal'c", "Uld'Gald"],
-      correct: 2,
-      timeLimit: 10,
-    },
-    {
-      text: "Who is the last French president of the 3rd Republic?",
-      answers: [
-        "Albert Lebrun",
-        "René Coty",
-        "Félix Faure",
-        "François Mitterrand",
-      ],
-      correct: 0,
-      timeLimit: 15,
-    },
-  ],
+	fr: [
+		{
+			text: "Quelle est la capitale de la France ?",
+			answers: ["Marseille", "Paris", "Lyon", "Bordeaux"],
+			correct: 1,
+			timeLimit: 10,
+		},
+		{
+			text: "Combien font 2 + 3 ?",
+			answers: ["3", "4", "5", "1"],
+			correct: 2,
+			timeLimit: 5,
+		},
+		{
+			text: "Quelle est la capitale de la Belgique ?",
+			answers: ["Bruxelles", "Anvers", "Liège", "Namur"],
+			correct: 0,
+			timeLimit: 10,
+		},
+		{
+			text: "Quelle est la couleur du cheval blanc d'Henry IV ?",
+			answers: ["Noir", "Blanc", "Rouge", "Vert", "Blanche"],
+			correct: 4,
+			timeLimit: 5,
+		},
+		{
+			text: "Quelle est la capitale de la Mongolie ?",
+			answers: ["Ulanbator", "Shanghai", "Sainte-Anne", "Omsk"],
+			correct: 0,
+			timeLimit: 10,
+		},
+		{
+			text: "Quel nombre n'est pas un nombre premier ?",
+			answers: ["2", "11", "17", "54"],
+			correct: 3,
+			timeLimit: 15,
+		},
+		{
+			text: "Comment s'appelle le protagoniste Goa'Uld dans l'équipage SG-1 ?",
+			answers: ["Jarod", "Spock", "Teal'c", "Uld'Gald"],
+			correct: 2,
+			timeLimit: 10,
+		},
+		{
+			text: "Qui est le dernier président de la 3eme république Française ?",
+			answers: [
+				"Albert Lebrun",
+				"René Coty",
+				"Félix Faure",
+				"François Mitterrand",
+			],
+			correct: 0,
+			timeLimit: 15,
+		},
+	],
+	en: [
+		{
+			text: "What is the capital of France?",
+			answers: ["Marseille", "Paris", "Lyon", "Bordeaux"],
+			correct: 1,
+			timeLimit: 10,
+		},
+		{
+			text: "How much is 2 + 3?",
+			answers: ["3", "4", "5", "1"],
+			correct: 2,
+			timeLimit: 5,
+		},
+		{
+			text: "What is the capital of Belgium?",
+			answers: ["Brussels", "Antwerp", "Liege", "Namur"],
+			correct: 0,
+			timeLimit: 10,
+		},
+		{
+			text: "What is the color of the white horse of Henry IV?",
+			answers: ["Black", "White", "Red", "Green"],
+			correct: 1,
+			timeLimit: 5,
+		},
+		{
+			text: "What is the capital of Mongolia?",
+			answers: ["Ulan Bator", "Shanghai", "Sainte-Anne", "Omsk"],
+			correct: 0,
+			timeLimit: 10,
+		},
+		{
+			text: "What is not a prime number?",
+			answers: ["2", "11", "17", "54"],
+			correct: 3,
+			timeLimit: 15,
+		},
+		{
+			text: "What is the name of the protagonist Goa'Uld in the SG-1 fleet?",
+			answers: ["Jarod", "Spock", "Teal'c", "Uld'Gald"],
+			correct: 2,
+			timeLimit: 10,
+		},
+		{
+			text: "Who is the last French president of the 3rd Republic?",
+			answers: [
+				"Albert Lebrun",
+				"René Coty",
+				"Félix Faure",
+				"François Mitterrand",
+			],
+			correct: 0,
+			timeLimit: 15,
+		},
+	],
 };
 
 let questions = [];
@@ -211,171 +212,177 @@ setText(bestScoreEnd, bestScore);
 
 const languageSelect = getElement("#language-select");
 languageSelect.addEventListener("change", () => {
-  applyTranslations(languageSelect.value);
+	applyTranslations(languageSelect.value);
 });
 
 applyTranslations(languageSelect.value);
 
 function applyTranslations(lang) {
-  const t = uiText[lang] || uiText["fr"];
+	const t = uiText[lang] || uiText["fr"];
 
-  document.title = t.title;
-  setText(getElement("h1"), t.title);
-  setText(getElement(".notice"), t.introNotice);
-  getElement("label[for='language-select']").textContent = t.selectLanguage;
-  setText(startBtn, t.start);
-  setText(nextBtn, t.next);
-  setText(restartBtn, t.restart);
-  setText(getElement("#result-screen h2"), t.resultTitle);
-  setText(scoreText, "");
+	document.title = t.title;
+	setText(getElement("h1"), t.title);
+	setText(getElement(".notice"), t.introNotice);
+	getElement("label[for='language-select']").textContent = t.selectLanguage;
+	setText(startBtn, t.start);
+	setText(nextBtn, t.next);
+	setText(restartBtn, t.restart);
+	setText(getElement("#result-screen h2"), t.resultTitle);
+	setText(scoreText, "");
 
-  const timerLabel = getElement("#timer-div label");
-  if (timerLabel) timerLabel.textContent = `${t.timeLeft} :`;
+	const timerLabel = getElement("#timer-div label");
+	if (timerLabel) timerLabel.textContent = `${t.timeLeft} :`;
 
-  bestScoreIntroLabel.textContent = t.bestScore + " : ";
-  bestScoreEndLabel.textContent = t.bestScore + " : ";
+	bestScoreIntroLabel.textContent = t.bestScore + " : ";
+	bestScoreEndLabel.textContent = t.bestScore + " : ";
 
-  // Met à jour les scores affichés
-  setText(bestScoreIntro, bestScore);
-  setText(bestScoreEnd, bestScore);
+	// Met à jour les scores affichés
+	setText(bestScoreIntro, bestScore);
+	setText(bestScoreEnd, bestScore);
+}
+
+export function sortQuestionsByTimeLimit(questions) {
+	return questions.sort((a, b) => a.timeLimit - b.timeLimit);
 }
 
 export function randomizeQuestions() {
-  for (let i = questions.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [questions[i], questions[j]] = [questions[j], questions[i]];
-  }
+	for (let i = questions.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[questions[i], questions[j]] = [questions[j], questions[i]];
+	}
 }
 
 function startQuiz() {
-  const selectedLang = languageSelect.value;
+	const selectedLang = languageSelect.value;
 
-  applyTranslations(selectedLang);
-  questions = translations[selectedLang] || translations["fr"];
+	applyTranslations(selectedLang);
+	questions = translations[selectedLang] || translations["fr"];
 
-  hideElement(introScreen);
-  showElement(questionScreen);
+	hideElement(introScreen);
+	showElement(questionScreen);
 
-  currentQuestionIndex = 0;
-  score = 0;
-  answeredQuestionsSummary = []; // Réinitialise le récapitulatif
+	currentQuestionIndex = 0;
+	score = 0;
+	answeredQuestionsSummary = []; // Réinitialise le récapitulatif
 
-  randomizeQuestions();
-  totalQuestionsSpan.textContent = questions.length;
+	randomizeQuestions();
+	sortQuestionsByTimeLimit(questions);
 
-  showQuestion();
+	totalQuestionsSpan.textContent = questions.length;
+
+	showQuestion();
 }
 
 function showQuestion() {
-  clearInterval(timerId);
+	clearInterval(timerId);
 
-  const q = questions[currentQuestionIndex];
-  setText(questionText, q.text);
-  setText(currentQuestionIndexSpan, currentQuestionIndex + 1);
+	const q = questions[currentQuestionIndex];
+	setText(questionText, q.text);
+	setText(currentQuestionIndexSpan, currentQuestionIndex + 1);
 
-  answersDiv.innerHTML = "";
-  q.answers.forEach((answer, index) => {
-    const btn = createAnswerButton(answer, () => selectAnswer(index, btn));
-    answersDiv.appendChild(btn);
-  });
+	answersDiv.innerHTML = "";
+	q.answers.forEach((answer, index) => {
+		const btn = createAnswerButton(answer, () => selectAnswer(index, btn));
+		answersDiv.appendChild(btn);
+	});
 
-  nextBtn.classList.add("hidden");
+	nextBtn.classList.add("hidden");
 
-  timeLeftSpan.textContent = q.timeLimit;
-  timerId = startTimer(
-    q.timeLimit,
-    (timeLeft) => setText(timeLeftSpan, timeLeft),
-    () => {
-      // Si le temps est écoulé, considérer comme une mauvaise réponse
-      const q = questions[currentQuestionIndex];
-      answeredQuestionsSummary.push({
-        question: q.text,
-        yourAnswer: uiText[languageSelect.value].timeLeft + " : 0s", // Ou une autre indication pour "pas de réponse"
-        correctAnswer: q.answers[q.correct],
-        isCorrect: false,
-      });
-      lockAnswers(answersDiv);
-      markCorrectAnswer(answersDiv, q.correct); // Afficher la bonne réponse même si le temps est écoulé
-      nextBtn.classList.remove("hidden");
-    }
-  );
+	timeLeftSpan.textContent = q.timeLimit;
+	timerId = startTimer(
+		q.timeLimit,
+		(timeLeft) => setText(timeLeftSpan, timeLeft),
+		() => {
+			// Si le temps est écoulé, considérer comme une mauvaise réponse
+			const q = questions[currentQuestionIndex];
+			answeredQuestionsSummary.push({
+				question: q.text,
+				yourAnswer: uiText[languageSelect.value].timeLeft + " : 0s", // Ou une autre indication pour "pas de réponse"
+				correctAnswer: q.answers[q.correct],
+				isCorrect: false,
+			});
+			lockAnswers(answersDiv);
+			markCorrectAnswer(answersDiv, q.correct); // Afficher la bonne réponse même si le temps est écoulé
+			nextBtn.classList.remove("hidden");
+		}
+	);
 }
 
 function selectAnswer(index, btn) {
-  clearInterval(timerId);
+	clearInterval(timerId);
 
-  const q = questions[currentQuestionIndex];
-  const isCorrect = index === q.correct;
-  if (isCorrect) {
-    score++;
-    btn.classList.add("correct");
-  } else {
-    btn.classList.add("wrong");
-  }
+	const q = questions[currentQuestionIndex];
+	const isCorrect = index === q.correct;
+	if (isCorrect) {
+		score++;
+		btn.classList.add("correct");
+	} else {
+		btn.classList.add("wrong");
+	}
 
-  answeredQuestionsSummary.push({
-    question: q.text,
-    yourAnswer: q.answers[index],
-    correctAnswer: q.answers[q.correct],
-    isCorrect: isCorrect,
-  });
+	answeredQuestionsSummary.push({
+		question: q.text,
+		yourAnswer: q.answers[index],
+		correctAnswer: q.answers[q.correct],
+		isCorrect: isCorrect,
+	});
 
-  markCorrectAnswer(answersDiv, q.correct);
-  lockAnswers(answersDiv);
-  nextBtn.classList.remove("hidden");
+	markCorrectAnswer(answersDiv, q.correct);
+	lockAnswers(answersDiv);
+	nextBtn.classList.remove("hidden");
 }
 
 function nextQuestion() {
-  currentQuestionIndex++;
-  if (currentQuestionIndex < questions.length) {
-    showQuestion();
-  } else {
-    endQuiz();
-  }
+	currentQuestionIndex++;
+	if (currentQuestionIndex < questions.length) {
+		showQuestion();
+	} else {
+		endQuiz();
+	}
 }
 
 function endQuiz() {
-  hideElement(questionScreen);
-  showElement(resultScreen);
+	hideElement(questionScreen);
+	showElement(resultScreen);
 
-  const selectedLang = languageSelect.value;
-  const t = uiText[selectedLang] || uiText["fr"];
+	const selectedLang = languageSelect.value;
+	const t = uiText[selectedLang] || uiText["fr"];
 
-  updateScoreDisplay(scoreText, score, questions.length, selectedLang);
+	updateScoreDisplay(scoreText, score, questions.length, selectedLang);
 
-  if (score > bestScore) {
-    bestScore = score;
-    saveToLocalStorage("bestScore", bestScore);
-  }
+	if (score > bestScore) {
+		bestScore = score;
+		saveToLocalStorage("bestScore", bestScore);
+	}
 
-  setText(bestScoreEnd, bestScore);
-  setText(bestScoreIntro, bestScore);
+	setText(bestScoreEnd, bestScore);
+	setText(bestScoreIntro, bestScore);
 
-  const bestScoreLabel = t.bestScore;
-  bestScoreEndLabel.textContent = bestScoreLabel + " : ";
-  bestScoreIntroLabel.textContent = bestScoreLabel + " : ";
+	const bestScoreLabel = t.bestScore;
+	bestScoreEndLabel.textContent = bestScoreLabel + " : ";
+	bestScoreIntroLabel.textContent = bestScoreLabel + " : ";
 
-  // Générer et afficher le tableau récapitulatif
-  const summaryTable = createSummaryTable(
-    answeredQuestionsSummary,
-    t.summaryTableTitle,
-    t.summaryQuestion,
-    t.summaryYourAnswer,
-    t.summaryCorrectAnswer
-  );
-  // Assurez-vous que le conteneur est vide avant d'ajouter le nouveau tableau
-  summaryTableContainer.innerHTML = "";
-  summaryTableContainer.appendChild(summaryTable);
+	// Générer et afficher le tableau récapitulatif
+	const summaryTable = createSummaryTable(
+		answeredQuestionsSummary,
+		t.summaryTableTitle,
+		t.summaryQuestion,
+		t.summaryYourAnswer,
+		t.summaryCorrectAnswer
+	);
+	// Assurez-vous que le conteneur est vide avant d'ajouter le nouveau tableau
+	summaryTableContainer.innerHTML = "";
+	summaryTableContainer.appendChild(summaryTable);
 }
 
 function restartQuiz() {
-  hideElement(resultScreen);
-  showElement(introScreen);
+	hideElement(resultScreen);
+	showElement(introScreen);
 
-  setText(bestScoreIntro, bestScore);
+	setText(bestScoreIntro, bestScore);
 
-  const selectedLang = languageSelect.value;
-  const bestScoreLabel = uiText[selectedLang]?.bestScore || "Meilleur score";
-  bestScoreIntroLabel.textContent = bestScoreLabel + " : ";
-  summaryTableContainer.innerHTML = ""; // Vide le tableau récapitulatif
+	const selectedLang = languageSelect.value;
+	const bestScoreLabel = uiText[selectedLang]?.bestScore || "Meilleur score";
+	bestScoreIntroLabel.textContent = bestScoreLabel + " : ";
+	summaryTableContainer.innerHTML = ""; // Vide le tableau récapitulatif
 }
